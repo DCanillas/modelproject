4eabcde069b9fd84bda34fb9b60e9f95186b0e45
Daniel_Canillas <Daniel_Canillas@epam.com>
2022-01-11 10:15:50 +0100

    remove unused import

--- src/main/java/org/example/modelproject/dto/ProductDTO.java
+++ src/main/java/org/example/modelproject/dto/ProductDTO.java
@@ -1,17 +1,13 @@
 package org.example.modelproject.dto;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-@ToString(exclude = "orders")
-@EqualsAndHashCode(exclude="orders")
 public class ProductDTO {
 
     private long id;
